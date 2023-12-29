d75e5cbd99f21f859d640f0d27ce94c355275051
Alexander Skjolden <alex@plastboks.net>
2023-12-29 11:10:32 +0100

    Simplifies build example

--- cmd/build_examples.go
+++ cmd/build_examples.go
@@ -9,28 +9,16 @@ import (
 )
 
 var examplesCmd = &cobra.Command{
-	Use:     "example",
-	Short:   "Examples found in cloud-config",
-	Long:    `Examples found in cloud-config`,
-	Aliases: []string{"examples"},
+	Use:   "example",
+	Short: "Builds example from cloud-config",
+	Long:  `Builds example example from cloud-config`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 		if err := InitGlobals(cmd); err != nil {
 			log.Fatalln(err)
 		}
 	},
-}
-
-var examplesInstallCmd = &cobra.Command{
-	Use:   "install",
-	Short: "install example from cloud-config",
-	Long:  `install example from cloud-config`,
 	Run: func(cmd *cobra.Command, args []string) {
-		exampleName, _ := cmd.Flags().GetString("example-name")
-		overrideGroupId, _ := cmd.Flags().GetString("group-id")
-		overrideArtifactId, _ := cmd.Flags().GetString("artifact-id")
-		overridePackage, _ := cmd.Flags().GetString("package")
-		overrideName, _ := cmd.Flags().GetString("name")
-
+		exampleName, _ := cmd.Flags().GetString("name")
 		if exampleName == "" {
 			log.Fatalln("please enter example --name")
 		}
@@ -68,47 +56,39 @@ var examplesInstallCmd = &cobra.Command{
 					log.Fatalln(err)
 				}
 
-				if overrideGroupId == "" {
-					groupId, err := promptFor("groupId", projectConfig.GroupId, force)
-					if err != nil {
-						log.Fatalln(err)
-					}
-					if err := cmd.Flags().Set("group-id", groupId); err != nil {
-						log.Fatalln(err)
-					}
+				groupId, err := promptFor("groupId", projectConfig.GroupId, force)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				if err := cmd.Flags().Set("group-id", groupId); err != nil {
+					log.Fatalln(err)
 				}
 
-				if overrideArtifactId == "" {
-					artifactId, err := promptFor("artifactId", projectConfig.ArtifactId, force)
-					if err != nil {
-						log.Fatalln(err)
-					}
-					if err := cmd.Flags().Set("artifact-id", artifactId); err != nil {
-						log.Fatalln(err)
-					}
+				artifactId, err := promptFor("artifactId", projectConfig.ArtifactId, force)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				if err := cmd.Flags().Set("artifact-id", artifactId); err != nil {
+					log.Fatalln(err)
 				}
 
-				if overridePackage == "" {
-					packageName, err := promptFor("package", projectConfig.Package, force)
-					if err != nil {
-						log.Fatalln(err)
-					}
-					if err := cmd.Flags().Set("package", packageName); err != nil {
-						log.Fatalln(err)
-					}
+				packageName, err := promptFor("package", projectConfig.Package, force)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				if err := cmd.Flags().Set("package", packageName); err != nil {
+					log.Fatalln(err)
 				}
 
-				if overrideName == "" {
-					name, err := promptFor("name", projectConfig.Name, force)
-					if err != nil {
-						log.Fatalln(err)
-					}
-					if err := cmd.Flags().Set("name", name); err != nil {
-						log.Fatalln(err)
-					}
-					if err := cmd.Flags().Set("application-name", fmt.Sprintf("%sApplication", name)); err != nil {
-						log.Fatalln(err)
-					}
+				name, err := promptFor("application-name", projectConfig.Name, force)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				if err := cmd.Flags().Set("name", name); err != nil {
+					log.Fatalln(err)
+				}
+				if err := cmd.Flags().Set("application-name", fmt.Sprintf("%sApplication", name)); err != nil {
+					log.Fatalln(err)
 				}
 
 				buildCmd.Run(cmd, args)
@@ -144,14 +124,11 @@ func init() {
 
 	examplesCmd.PersistentFlags().StringVar(&ctx.TargetDirectory, "target", ".", "Optional target directory")
 	examplesCmd.PersistentFlags().BoolVar(&ctx.DryRun, "dry-run", false, "dry run does not write to pom.xml")
+	examplesCmd.Flags().String("name", "", "Example name to use")
 
-	examplesCmd.AddCommand(examplesInstallCmd)
-	examplesInstallCmd.Flags().StringP("example-name", "n", "", "Example name")
-
-	examplesInstallCmd.Flags().String("boot-version", "", "Defines spring-boot version to use")
-	examplesInstallCmd.Flags().String("group-id", "", "Overrides groupId from config file")
-	examplesInstallCmd.Flags().String("artifact-id", "", "Overrides artifactId from config file")
-	examplesInstallCmd.Flags().String("package", "", "Overrides package from config file")
-	examplesInstallCmd.Flags().String("name", "", "Overrides name from config file")
-	examplesInstallCmd.Flags().String("application-name", "", "Overrides application-name from config file")
+	examplesCmd.Flags().String("boot-version", "", "Defines spring-boot version to use")
+	//examplesCmd.Flags().String("group-id", "", "Overrides groupId from config file")
+	//examplesCmd.Flags().String("artifact-id", "", "Overrides artifactId from config file")
+	//examplesCmd.Flags().String("package", "", "Overrides package from config file")
+	//examplesCmd.Flags().String("application-name", "", "Overrides application-name from config file")
 }
